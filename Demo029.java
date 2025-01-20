c2794c0a4ff2895f220923acbf8c1dc082cb9b1f
Gaddam Samvedha <gaddamsamvedha@gmail.com>
2025-01-20 12:32:58 +0530

    Array list to store 10 friends names

---
+++ Demo029.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Demo029{
+    public static void main(String[] args) {
+    
+        ArrayList<String> friends = new ArrayList<>();
+        
+        friends.add("Anu");
+        friends.add("Bindu");
+        friends.add("Chinni");
+        friends.add("Dhanu");
+        friends.add("Eva");
+        friends.add("Fruity");
+        friends.add("Gangu");
+        friends.add("Honey");
+        friends.add("Ishu");
+        friends.add("Jhanu");
+        
+        System.out.println("Friends List: " + friends);
+    }
+}
