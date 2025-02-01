e51ff9621d221db3de65718b12fd2b6e254c58ad
gaddamsamvedha <gaddamsamvedha@gmail.com>
2025-02-01 13:19:47 +0530

    Greatest of three

---
+++ PDemo003.java
@@ -0,0 +1,17 @@
+public class PDemo003 {
+    public static void main(String[] args) {
+        int num1,num2,num3;
+        num1=10;
+        num2=36;
+        num3=8;
+        if(num1>num2){
+            System.err.println("num1 is greater");
+        }
+        else if(num2>num3){
+            System.err.println("num2 is greater");
+        }
+        else{
+            System.out.println("num3 is greater");
+        }
+    }
+}
