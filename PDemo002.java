b3c42b4c4d1b8ed9610e993e33c3a8b2a2c147c0
gaddamsamvedha <gaddamsamvedha@gmail.com>
2025-02-01 13:18:05 +0530

    Greatest of two

---
+++ PDemo002.java
@@ -0,0 +1,14 @@
+public class PDemo002{
+    public static void main(String[]args){
+        int num1,num2;
+         num1=23;
+         num2=45;
+         if(num1>num2){
+            System.out.println("num1 is greater");
+         }
+         else{
+            System.out.println("num2 is greater");
+         }
+
+    }
+}
