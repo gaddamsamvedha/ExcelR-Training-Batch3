ea9a17768816995ba3be89b110d52fdfde60522d
Gaddam Samvedha <gaddamsamvedha@gmail.com>
2025-02-11 12:29:38 +0530

    To sort an array without predefined methods

--- Demo074.java
+++ Demo074.java
@@ -1,23 +1,31 @@
 public class Demo074 {
-        public static void main(String[] args) {
-            int[] arr = {64, 34, 25, 12, 22, 11, 90};
-            bubbleSort(arr);
-            System.out.println("Sorted array:");
-            for (int i = 0; i < arr.length; i++) {
-                System.out.print(arr[i] + " ");
-            }
-        }
-        public static void bubbleSort(int[] arr) {
-            int n = arr.length;
-            for (int i = 0; i < n - 1; i++) {
-                for (int j = 0; j < n - 1 - i; j++) {
-                    if (arr[j] > arr[j + 1]) {
-                        int temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
-                    }
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90}; 
+        
+        System.out.println("Original array: ");
+        printArray(arr);
+    
+        sort(arr);
+        
+        System.out.println("Sorted array: ");
+        printArray(arr);
+    }
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
     }
-    
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();  
+    }
+}
